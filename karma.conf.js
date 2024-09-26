5a36e562f5bb3df4d2b24125b6d72e00906c3c02
AlexanderCastilloMurga <159457647+AlexanderCastilloMurga@users.noreply.github.com>
2024-09-26 17:43:45 -0600

    Update karma.conf.js (#18)

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/unit-testing'),
+      dir: require('path').join(__dirname, './coverage/mycv'),
       subdir: '.',
       reporters: [
         { type: 'html' },
@@ -33,19 +33,15 @@ module.exports = function (config) {
       ]
     },
     reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
     browsers: ['Chrome'],
     browsers: ['ChromeHeadlessCI'],
-     customLaunchers: {
-       ChromeHeadlessCI: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-       }
-    },
-    singleRun: false,
+        customLaunchers: {
+          ChromeHeadlessCI: {
+           base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+         }
+      },
+      singleRun:false,
     restartOnFileChange: true
   });
 };
